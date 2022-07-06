50bb58c8a462a9494de9cc0794f6353f0f570436
Arechi <22101241+Arechii@users.noreply.github.com>
2022-07-06 17:27:40 +0200

    Wrap app elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import ProcedureForm from "./components/ProcedureForm";
 import { Box } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import ProcedureDesign from "./components/ProcedureDesign";
-import  Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-    >
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<ProcedureForm />} />
-        <Route path="design" element={<ProcedureDesign />} />
-      </Routes>
-    </Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Routes>
+          <Route path="/" element={<ProcedureForm />} />
+          <Route path="design" element={<ProcedureDesign />} />
+        </Routes>
+      </Box>
+    </>
   );
 }
