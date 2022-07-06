6f9265d217aa3470f2cb7967a4514228bb83aa38
Arechi <22101241+Arechii@users.noreply.github.com>
2022-07-06 16:47:34 +0200

    Start work on drag & drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import ProcedureForm from "./components/ProcedureForm";
-import { Container, Box } from "@mui/material";
+import { Box } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import ProcedureDesign from "./components/ProcedureDesign";
 
 export default function App() {
   return (
-    <Container maxWidth="md">
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
-        <Routes>
-          <Route path="/" element={<ProcedureForm />} />
-          <Route path="design" element={<ProcedureDesign />} />
-        </Routes>
-      </Box>
-    </Container>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Routes>
+        <Route path="/" element={<ProcedureForm />} />
+        <Route path="design" element={<ProcedureDesign />} />
+      </Routes>
+    </Box>
   );
 }
--- src/components/ProcedureDesign.tsx
+++ src/components/ProcedureDesign.tsx
@@ -1,3 +1,52 @@
+import { Box, Divider, Grid } from "@mui/material";
+import { DragDropContext, Draggable } from "react-beautiful-dnd";
+import Droppable from "./Droppable";
+
 export default function ProcedureDesign() {
-  return <div>Design</div>;
+  return (
+    <Grid container spacing={3} height="100vh" width="100vw" m={0}>
+      <Grid item xs>
+        <span>Steps</span>
+        <DragDropContext onDragEnd={() => {}}>
+          <Droppable droppableId="droppable">
+            {(provided) => (
+              <div ref={provided.innerRef}>
+                {[
+                  { id: "1", content: "Step 1" },
+                  { id: "2", content: "Step 2" },
+                ].map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided) => (
+                      <Box
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        sx={{
+                          borderRadius: 1,
+                          border: 2,
+                        }}
+                        p={2}
+                        m={2}
+                      >
+                        {item.content}
+                      </Box>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </Grid>
+      <Divider orientation="vertical" />
+      <Grid item xs={6}>
+        <span>Design</span>
+      </Grid>
+      <Divider orientation="vertical" />
+      <Grid item xs>
+        <span>Configure</span>
+      </Grid>
+    </Grid>
+  );
 }
