49a999e277163b0c2922842edb274377deca52f5
wari <nicolaycamacho@gmail.com>
2022-07-18 16:27:14 +0200

    Builder main container added, icons missing

--- src/components/Builder.tsx
+++ src/components/Builder.tsx
@@ -1,11 +1,12 @@
 // import {Book} from '@mui/icons-material/book';
 // import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
-import TextField from './TextField';
+
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import BuilderHelper from './BuilderHelper';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import FieldsLibrary from './FieldsLibrary';
+import Button from '@mui/material/Button';
+import BuilderMainContainer from './BuilderMainContainer';
 
 export default function ProcedureDesign() {
   return (
@@ -14,22 +15,11 @@ export default function ProcedureDesign() {
         <FieldsLibrary />
       </div>
       <div className="builder-main">
-        <div className='builder-title-section'>
-          <h1>Contact Details</h1>
-        </div>
         <div className='builder-component-section'>
-          <Box sx={{width:'35%',border:1, marginBottom:'30px'}}>
-            <TextField />
-          </Box>
-          <Box sx={{width:'35%',border:1, marginBottom:'30px'}}>
-            <TextField />
-          </Box>
-          <Box sx={{width:'35%',border:1, marginBottom:'30px'}}>
-            <TextField />
-          </Box>
+          <BuilderMainContainer />
         </div>
         <Box sx={{display:'flex', justifyContent:'center'}}>
-          <AddCircleOutlineIcon sx={{width:'60px'}} />
+          <Button variant='contained' color='primary' size='large'>+ Create new field</Button>
         </Box>
       </div>
       <div className="builder-helper">
